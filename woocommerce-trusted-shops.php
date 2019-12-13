90852a83abdffa580efd754296a32d88d75b7c60
Dennis Nissle <info@vendidero.de>
2019-12-13 13:47:00 +0100

    reverted back to missing 4.0.3

--- woocommerce-trusted-shops.php
+++ woocommerce-trusted-shops.php
@@ -3,7 +3,7 @@
  * Plugin Name: Trustbadge Reviews for WooCommerce
  * Plugin URI: http://www.trustedshops.co.uk/
  * Description: Adds Seller and Product Reviews or Trusted Shops Integration to your WooCommerce Shop.
- * Version: 4.0.4
+ * Version: 4.0.3
  * Author: Vendidero
  * Author URI: http://vendidero.de
  * Requires at least: 4.9
