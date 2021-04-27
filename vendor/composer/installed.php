adbf9c35120a52bb8fd5119834f79f4cc50696fd
Dennis Nissle <info@vendidero.de>
2021-04-27 14:31:02 +0200

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '112e08affde43c4770893b0cb07a4fe0d93d3812',
+    'name' => 'vendidero/woocommerce-trusted-shops',
+  ),
+  'versions' => 
+  array (
+    'automattic/jetpack-autoloader' => 
+    array (
+      'pretty_version' => '2.10.1',
+      'version' => '2.10.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '20393c4677765c3e737dcb5aee7a3f7b90dce4b3',
+    ),
+    'vendidero/woocommerce-trusted-shops' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '112e08affde43c4770893b0cb07a4fe0d93d3812',
+    ),
+  ),
+);
