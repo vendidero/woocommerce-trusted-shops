cf69807b43c7ba706af2efeba4788748af35f7b7
PW <pw@pw.is>
2021-06-02 08:50:06 +0200

    modified code to take advantage of WordPress core scripts enqueue mechanism

--- templates/trusted-shops/trustbadge-tpl.php
+++ templates/trusted-shops/trustbadge-tpl.php
@@ -4,26 +4,29 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+  exit; // Exit if accessed directly
 }
 ?>
 
 <script type="text/javascript">
 (function () {
-	var _tsid = '{id}'; 
-	_tsConfig = {
-		'yOffset': '{offset}', /* offset from page bottom */
-		'variant': '{variant}', /* reviews, default, custom, custom_reviews */
-		'customElementId': '', /* required for variants custom and custom_reviews */
-		'trustcardDirection': '', /* for custom variants: topRight, topLeft, bottomRight, bottomLeft */ 'customBadgeWidth': '', /* for custom variants: 40 - 90 (in pixels) */
-		'customBadgeHeight': '', /* for custom variants: 40 - 90 (in pixels) */
-		'disableResponsive': 'false', /* deactivate responsive behaviour */
-		'disableTrustbadge': '{disable}', /* deactivate trustbadge */
-		'trustCardTrigger': 'mouseenter', /* set to 'click' if you want the trustcard to be opened on click instead */ 'customCheckoutElementId': ''/* required for custom trustcard */
-	};
-	var _ts = document.createElement('script');
-	_ts.type = 'text/javascript';
-	_ts.charset = 'utf-8';
-	_ts.async = true;
-	_ts.src = '//widgets.trustedshops.com/js/' + _tsid + '.js'; var __ts = document.getElementsByTagName('script')[0]; __ts.parentNode.insertBefore(_ts, __ts);
-})();</script>
+  var _tsid = '{id}';
+  _tsConfig = {
+    'yOffset': '{offset}', /* offset from page bottom */
+    'variant': '{variant}', /* reviews, default, custom, custom_reviews */
+    'customElementId': '', /* required for variants custom and custom_reviews */
+    'trustcardDirection': '', /* for custom variants: topRight, topLeft, bottomRight, bottomLeft */ 'customBadgeWidth': '', /* for custom variants: 40 - 90 (in pixels) */
+    'customBadgeHeight': '', /* for custom variants: 40 - 90 (in pixels) */
+    'disableResponsive': 'false', /* deactivate responsive behaviour */
+    'disableTrustbadge': '{disable}', /* deactivate trustbadge */
+    'trustCardTrigger': 'mouseenter', /* set to 'click' if you want the trustcard to be opened on click instead */ 'customCheckoutElementId': ''/* required for custom trustcard */
+  };
+})();
+</script>
+<?php
+/*
+ * Load Script through WordPress Core function
+ * load in footer for increase Core Web Vitals
+ */
+wp_enqueue_script( 'script', '//widgets.trustedshops.com/js/{id}.js', [], 1.0, true);
+?>
